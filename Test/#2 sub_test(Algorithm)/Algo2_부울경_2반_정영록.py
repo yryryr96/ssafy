952e5b579c393a179f3abce122ac45281435baca
정영록 <s9285s@naver.com>
2023-02-13 11:15:38 +0900

    test

--- Test/#2 sub_test(Algorithm)/Algo2_부울경_2반_정영록.py
+++ Test/#2 sub_test(Algorithm)/Algo2_부울경_2반_정영록.py
@@ -7,8 +7,8 @@ for tc in range(1,T+1):
     for _ in range(N):
         A,B,K = map(int,input().split()) # A,B : 두더지의 좌표, K : 두더지가 몇초동안 유지 되는지
         lst[A][B] = 1
-        k = 0 ; n = 0 # k : 가로로 몇번 움직였는지 , n : 세로로 몇 번 움직였는지
-        while k < K : # 가로로 움직인 횟수가 K번보다 작을 때
+        k = 0 # 움직임 횟수
+        while k < K : # 움직인 횟수가 K번보다 작을 때
             k += 1
             if c < B :  # 망치의 열의 좌표가 두더지 좌표보다 작다면
                 c = c + 1
@@ -23,8 +23,8 @@ for tc in range(1,T+1):
             if c == B : # K번 내에 열의 좌표가 같아진다면 break 하고 행 좌표 변환
                 break
 
-        while n+k < K : # 가로+세로로 움직인 횟수가 K번 보다 작을 때
-            n+=1
+        while k < K : # 움직인 횟수가 K번 보다 작을 때
+            k+=1
             if r < A :  # 망치의 행의 좌표가 A보다 작을 때
                 r = r + 1
 
