8bf1861f9c9ce98e13208a444bc07c1fb028bee7
정영록 <s9285s@naver.com>
2023-03-12 22:05:48 +0900

    4th study

---
+++ study/4th/택배 배송.py
@@ -0,0 +1,31 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+start = 1
+INF = int(1e9)
+graph = [[] for _ in range(n+1)]
+distance = [INF for _ in range(n+1)]
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    graph[a].append((b,c))
+    graph[b].append((a,c))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q,(0,start))
+    distance[start] = 0
+    while q:
+        dist, node = heapq.heappop(q)
+        if distance[node] < dist :
+            continue
+        for after in graph[node] :
+            cost = distance[node] + after[1]
+            if cost < distance[after[0]] :
+                distance[after[0]] = cost
+                heapq.heappush(q,(cost,after[0]))
+
+dijkstra(start)
+print(distance[n])
+
---
+++ study/4th/팀배분.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+visited = [0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+
+for i in range(1,n+1):
+    lst = list(map(int,input().split()))
+    l = lst[0]
+    for j in range(1,l+1):
+        if lst[j] not in graph[i] :
+            graph[i].append(lst[j])
+        if i not in graph[lst[j]] :
+            graph[lst[j]].append(i)
+
+print(graph)
+def dfs(v):
