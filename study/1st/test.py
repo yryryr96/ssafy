09632523a2c80f2693a0f8a5afabc9997bd3b83b
정영록 <s9285s@naver.com>
2023-03-14 08:50:28 +0900

    1st study

--- study/1st/test.py
+++ study/1st/test.py
@@ -1,21 +1,40 @@
-from collections import deque
-from itertools import product
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-M = int(input())
-broken = list(map(str,input().split()))
-cnt = abs(100-N)    # 100채널에서 한칸씩 목표 채널로 가는 횟수
+n = int(input())
+visited = [0 for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
 
-for i in range(1000001): # 최대 한칸씩 움직일때 위아래로 50만 일수 있으니
-    temp = 1
-    for j in str(i):
-        if j in broken:
-            temp = 0
-            break
-    if temp == 1:
-        cnt = min(cnt,abs(i-N)+len(str(i)))
+for i in range(1,n+1):
+    lst = list(map(int,input().split()))
+    l = lst[0]
+    for j in range(1,l+1):
+        if lst[j] not in graph[i] :
+            graph[i].append(lst[j])
+        if i not in graph[lst[j]] :
+            graph[lst[j]].append(i)
 
-# 이동가능한 채널로 이동하기 위해서 누른 버튼횟수와 그 채널에서 목표 타겟까지 한칸씩 움직이는 횟수
-print(cnt)
+white = []
+white_check = []
+blue = []
+def dfs(v):
+    visited[v] = 1
+    white.append(v)
+    for a in graph[v] :
+        if a not in white_check:
+            white_check.append(a)
+
+    for i in range(1,n+1):
+        if i not in white_check and visited[i] == 0 :
+            dfs(i)
+
+dfs(1)
+
+for i in range(1,n+1):
+    if i not in white:
+        blue.append(i)
+
+print(len(white))
+print(*white)
+print(len(blue))
+print(*blue)
