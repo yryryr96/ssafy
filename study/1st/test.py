04fa7f6d5b3ab6e1bb83e15bbc56bf7e8bf78d1f
정영록 <s9285s@naver.com>
2023-03-15 21:36:21 +0900

    study

--- study/1st/test.py
+++ study/1st/test.py
@@ -1,31 +1,29 @@
-import sys
-import heapq
+import heapq,sys
 input = sys.stdin.readline
 
 n,m = map(int,input().split())
-start = 1
-INF = int(1e9)
-graph = [[] for _ in range(n+1)]
-distance = [INF for _ in range(n+1)]
-for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
-    graph[b].append((a,c))
+q = list(map(int,input().split()))
+q.sort()
+ans = 0
+while q :
+    A = q.pop()
+    temp = A
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q,(0,start))
-    distance[start] = 0
-    while q:
-        dist, node = heapq.heappop(q)
-        if distance[node] < dist :
-            continue
-        for after in graph[node] :
-            cost = distance[node] + after[1]
-            if cost < distance[after[0]] :
-                distance[after[0]] = cost
-                heapq.heappush(q,(cost,after[0]))
+    if not q :
+        ans+=temp
+    while A != 0 and q :
+        for _ in range(m-1):
+            B = q.pop()
+            A -= B
+            if A == 0 :
+                ans += temp
+                temp = 0
+                break
+        if not q:
+            ans += temp
+            break
 
-dijkstra(start)
-print(distance[n])
 
+
+
+print(ans)
