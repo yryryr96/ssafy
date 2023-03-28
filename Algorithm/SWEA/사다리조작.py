59e5e6f5645d4d1fa826413026d261c6adf18fca
정영록 <s9285s@naver.com>
2023-03-29 00:49:02 +0900

    study

--- Algorithm/SWEA/사다리조작.py
+++ Algorithm/SWEA/사다리조작.py
@@ -4,67 +4,49 @@ input = sys.stdin.readline
 n,m,h = map(int,input().split())
 graph = [[0]*(n+1) for _ in range(h+1)]
 
-def check(): # 가로 선 개수 세는 함수
-    row_check = [0] * (n + 1)
-    for j in range(1,n):
-        for i in range(1,h+1):
-            if graph[i][j] == 1:
-                row_check[i] += 1
-    if max(row_check) < m :
-        return True
-    else :
-        return False
-
 def move():
     for j in range(1,n+1):
-        i = 1
-        visited = [[0]*(n+1) for _ in range(h+1)]
-        row,col = i,j
-        while True :
-            visited[row][col] = 1
-            if row == h :
-                if col != j :
-                    return False
-                break
-            if graph[row][col] == 1 and visited[row][col+1] == 0:
+        col = j
+        for row in range(1,h+1):
+            if graph[row][col] :
                 col += 1
-            else:
-                if graph[row][col-1] == 1 and visited[row][col-1] == 0 :
-                    col-=1
-                else:
-                    row += 1
+            elif graph[row][col-1] :
+                col -= 1
+        if col != j :
+            return False
     return True
 
-for i in range(m):
+for _ in range(m):
     a,b = map(int,input().split())
     graph[a][b] = 1
 
-for i in range(h+1):
-    print(*graph[i])
-
-def dfs(i,j,cnt):
-    if j >= n+1 :
-        i += 1
-        j = 1
-
-    if i == h + 1 :
-        return
+MIN = sys.maxsize
+def dfs(row,col,cnt):
+    global MIN
 
     if move():
-        return cnt
+        MIN = min(MIN,cnt)
+        return MIN
 
-    if graph[i][j] == 0 :
-        if (j+1 <= n and graph[i][j+1] != 1) and (graph[i][j-1] != 1) and check:
-            print(i, j,cnt)
-            graph[i][j] = 1
-            dfs(i,j+1,cnt+1)
-            graph[i][j] = 0
+    if cnt == 3 or cnt >= MIN:
+        return
 
-    dfs(i,j+1,cnt)
+    for i in range(row,h+1):
+        if i == row :
+            col = col
+        else :
+            col = 1
+        for j in range(col,n):
+            if graph[i][j] == 0 and graph[i][j - 1] == 0 and graph[i][j+1] == 0 :
+                graph[i][j] = 1
+                dfs(i,j+1, cnt + 1)
+                graph[i][j] = 0
 
+dfs(1,1,0)
 
+if MIN > 3 :
+    print(-1)
 
-print(dfs(1,1,0))
-print()
-for i in range(h+1):
-    print(*graph[i])
+else:
+    print(MIN)
+
--- study/6th/넴모넴모(easy).py
+++ study/6th/넴모넴모(easy).py
@@ -8,21 +8,23 @@ ans = 1
 
 def dfs(i,j):
     global ans
-    if j == m+1 :
+    if j == m+1 :   # 열 끝으로 이동하면 행,열 좌표 이동
         i += 1
         j = 1
 
-    if i == n+1 :
+    if i == n+1 :   # 행이 끝까지 넘어가면 리턴
        return 0
 
     if graph[i][j-1] == 0 or graph[i-1][j] == 0 or graph[i-1][j-1] == 0 :
+        #오른쪽 아래 좌표 기준 네모가 아니라면
         graph[i][j] = 1
         ans += 1
         dfs(i,j+1)
         graph[i][j] = 0
 
     dfs(i,j+1)
-    return ans
+    # 네모이면 다음 좌표 탐색
+    return
 
 dfs(1,1)
 print(ans)
