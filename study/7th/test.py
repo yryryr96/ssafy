084977491a29e7e36ce20423febac448207a7547
정영록 <s9285s@naver.com>
2023-04-03 23:16:55 +0900

    study

--- study/7th/test.py
+++ study/7th/test.py
@@ -1,8 +1,30 @@
-T = int(input())
+import sys
+input = sys.stdin.readline
 
-for tc in range(1,T+1):
-    n, number = input().split()
-    number = bin(int(number,16))[2:]
-    if len(number)%4 != 0 :
-        number = '0'*(4-len(number)%4) + number
-    print(f'#{tc} {number}')
+def find(x):
+    if parent[x] != x :
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(a,b):
+    a = find(a)
+    b = find(b)
+    parent[max(a,b)] = min(a,b)
+
+n,m = map(int,input().split())
+parent = list(range(n+1))
+edges = []
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    edges.append((c,a,b))
+edges.sort()
+ans = 0
+MAX = 0
+for i in range(m):
+    cost,a,b = edges[i]
+    if find(a) != find(b):
+       union(a,b)
+       ans += cost
+       MAX = max(MAX,cost)
+
+print(ans-MAX)
