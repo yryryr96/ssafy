2b0f053e1eb9a550a6475d697fdb4eafbf69133e
정영록 <s9285s@naver.com>
2023-04-07 17:50:25 +0900

    study

--- study/8th/test.py
+++ study/8th/test.py
@@ -1,32 +1,13 @@
+from collections import deque
 import sys
 input = sys.stdin.readline
 
-n,k = map(int, input().split())
-lst = list(map(int, input().split()))
-temp = []
-for i in range(k):
-    temp.append([lst[i],i+1])
+n,m = map(int,input().split())
+graph = [[0]*(n+1) for _ in range(m+1)]
+for _ in range(4):
+    x,y = map(int,input().split())
+    graph[y][x] = 1
 
-temp.sort(reverse=True)
+for i in range(m+1):
+    print(*graph[i])
 
-if max(lst) -1 > sum(lst) - max(lst) :
-    print(-1)
-    exit()
-else:
-    ans = [0] * n
-    j = 0
-    for i in range(0,n,2):
-        ans[i] = temp[j][1]
-        temp[j][0] -= 1
-        if temp[j][0] == 0 :
-            j+=1
-
-    i = 1
-    while i < n :
-        ans[i] = temp[j][1]
-        temp[j][0] -= 1
-        if temp[j][0] == 0 :
-            j += 1
-        i+=2
-
-    print(*ans)
