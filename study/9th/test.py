724625575cc9035c8a3b28947a801d25d124ef9a
정영록 <s9285s@naver.com>
2023-04-17 23:25:08 +0900

    study

--- study/9th/test.py
+++ study/9th/test.py
@@ -1,30 +1,52 @@
-
-import sys,math
+import sys,copy
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-
-A = list(map(int,input().split()))
-B = list(map(int,input().split()))
-lst = []
-
+graph = [list(map(int,input().split())) for _ in range(n)]
+point = [[0,1],[1,0],[-1,0],[0,-1]]
+MAX = 0
 for i in range(n):
-    lst.append([A[i],B[i]])
-lst.sort(key = lambda x: (x[1],x[0]))
+    MAX = max(MAX,max(graph[i]))
+
+def bfs(h):
+    temp = copy.deepcopy(graph)
+    visited = [[0]*n for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
+            if temp[i][j] <= h :
+                temp[i][j] = 0
+
+    def island(i,j,a):
+        q = deque()
+        q.append((i,j))
+        while q:
+            i,j = q.popleft()
+            for di,dj in point:
+                ni,nj = i+di,j+dj;
+                if 0<=ni<n and 0<=nj<n and temp[ni][nj]!= 0 and visited[ni][nj] == 0:
+                    visited[ni][nj] = 1
+                    q.append((ni,nj))
+                    temp[ni][nj] = a
+    a = 0
+    for i in range(n):
+        for j in range(n):
+            if temp[i][j] != 0 and visited[i][j] == 0 :
+                a+=1
+                visited[i][j] = 1
+                island(i,j,a)
+
+    return a
 
 ans = 0
-MAX = 0
-temp = lst[0][1]
-for i in range(n):
-
-    if lst[i][0] < temp :
-        temp = max(temp,lst[i][1])
-        k = math.ceil((temp-lst[i][0])/30)
-        lst[i][0] += k*30
-        ans += k
-
-    MAX = max(MAX,lst[i][0])
-    if i+1 < n and lst[i+1][1] != lst[i][1] :
-        temp = MAX
-
+for i in range(MAX+1):
+    k = bfs(i)
+    if ans < k :
+        ans = k
 print(ans)
+
+
+
+
+
+
