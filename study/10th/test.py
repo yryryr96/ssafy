cbf9bb20553c5c01b78f97ab0971e4a6ddf594c9
정영록 <s9285s@naver.com>
2023-04-19 21:46:32 +0900

    study

--- study/10th/test.py
+++ study/10th/test.py
@@ -5,18 +5,46 @@ input = sys.stdin.readline
 n,m = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(n)]
 point = [[0,1],[1,0],[-1,0],[0,-1]]
-
+ans = 0
 def bfs():
+    global ans
+    visited = [[0]*m for _ in range(n)]
     q = deque()
+    q.append((0,0))
+    visited[0][0] = 1
+    edges = []
 
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                for di,dj in point :
-                    ni,nj = i+di,j+dj
-                    if 0<=ni<n and 0<=nj<m and graph[ni][nj] == 0 :
-                        q.append((ni,nj))
-                        break
+    while q:
+        i,j = q.popleft()
+        for di,dj in point :
+            ni,nj = i+di, j+dj
+            if 0<=ni<n and 0<=nj<m and visited[ni][nj] == 0 :
+                visited[ni][nj] = 1
+                if graph[ni][nj] == 0 :
+                    q.append((ni,nj))
 
-    for i,j in q:
-        graph[i][j] = 0
+                elif graph[ni][nj] == 1 :
+                    edges.append((ni,nj))
+
+    for r,c in edges :
+        graph[r][c] = 0
+        ans -= 1
+
+    return
+
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1 :
+            ans += 1
+
+cnt = 0
+temp = 0
+while True:
+    cnt += 1
+    bfs()
+    if ans == 0 :
+        break
+    temp = ans
+
+print(cnt)
+print(temp)
