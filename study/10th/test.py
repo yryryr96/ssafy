a06bec0f574d2af3ceaa02dffae3d9b277f53e66
정영록 <s9285s@naver.com>
2023-04-20 17:56:15 +0900

    study

--- study/10th/test.py
+++ study/10th/test.py
@@ -1,22 +1,45 @@
 import sys
-from collections import deque
+from itertools import combinations
 input = sys.stdin.readline
 
-n,m = map(int,input().split())
-graph = [list(map(int,input().split())) for _ in range(n)]
-point = [[0,1],[1,0],[-1,0],[0,-1]]
 
-def bfs():
-    q = deque()
+lst = list(map(int,input().split()))
+n = lst[0]
+nowx,nowy = lst[1],lst[2]
+bombx,bomby = lst[3],lst[4]
 
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1:
-                for di,dj in point :
-                    ni,nj = i+di,j+dj
-                    if 0<=ni<n and 0<=nj<m and graph[ni][nj] == 0 :
-                        q.append((ni,nj))
-                        break
+dx = bombx-nowx
+dy = bomby-nowy
+move_cnt = (dx+dy) // n
+temp_move = (dx+dy) % n
 
-    for i,j in q:
-        graph[i][j] = 0
+if dx < 0 or dy < 0 :
+    print(-1)
+    exit()
+
+a = 0
+b = n
+check = []
+ans = []
+while b >= 0 :
+    k1 = a * move_cnt
+    k2 = b * move_cnt
+    if k1 > dx or k2 > dy :
+        pass
+    else :
+        result = ""
+        p1 = dx-k1
+        p2 = dy-k2
+        if p1 >= 0 and p2 >= 0 and a-p1>=0 and b-p2 >= 0 :
+            result = 'R' * (p1) + 'U' * (p2) + 'R' * (a - p1) + 'U' * (b - p2)
+            check.append((a-p1,b-p2,p1,p2))
+            ans.append(result)
+    a+=1
+    b-=1
+
+ans.sort()
+if not check:
+    print(-1)
+else:
+    print(ans[0])
+
