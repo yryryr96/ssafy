acabc1e55737fdfd5f1886256e3d34c7dfb392ee
정영록 <s9285s@naver.com>
2023-04-27 23:43:43 +0900

    study

--- study/11th/test.py
+++ study/11th/test.py
@@ -1,24 +1,43 @@
-import sys
+import sys,heapq
 input = sys.stdin.readline
 
 T = int(input())
-for tc in range(T) :
-    ability = [list(map(int,input().split())) for _ in range(11)]
-    position = [0]*11
-    MAX = 0
-    def dfs(i,p):
-        global MAX
-        if i == 11 :
-            if MAX < sum(p) :
-                MAX = sum(p)
-            return
+for _ in range(T):
+    k = int(input())
+    max_heap = []
+    min_heap = []
+    visited = [0]*k
+    for id in range(k):
+        command,num = input().rstrip().split()
+        num = int(num)
 
-        for k in range(11) :
-            if p[k] == 0 and ability[i][k] != 0:
-                p[k] = ability[i][k]
-                dfs(i+1,p)
-                p[k] = 0
+        if command == 'I' :
+            heapq.heappush(max_heap,(-num,id))
+            heapq.heappush(min_heap,(num,id))
+            visited[id] = 1
 
-    dfs(0,position)
-    print(MAX)
+        else:
+            if num == - 1:
+                while min_heap and not visited[min_heap[0][1]] :
+                    heapq.heappop(min_heap)
+                if min_heap :
+                    visited[min_heap[0][1]] = 0
+                    heapq.heappop(min_heap)
 
+            elif num == 1 :
+                while max_heap and not visited[max_heap[0][1]] :
+                    heapq.heappop(max_heap)
+                if max_heap :
+                    visited[max_heap[0][1]] = 0
+                    heapq.heappop(max_heap)
+
+    while min_heap and not visited[min_heap[0][1]]:
+        heapq.heappop(min_heap)
+
+    while max_heap and not visited[max_heap[0][1]]:
+        heapq.heappop(max_heap)
+
+    if not min_heap or not max_heap :
+        print('EMPTY')
+    else:
+        print(-max_heap[0][0],min_heap[0][0])
