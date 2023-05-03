475dd3fdbdd3e69bc5dd83bb31119b5bf14f0605
정영록 <s9285s@naver.com>
2023-05-03 21:46:25 +0900

    study

--- study/12th/드래곤 앤 던전.py
+++ study/12th/드래곤 앤 던전.py
@@ -1,10 +1,34 @@
 import sys
 input = sys.stdin.readline
 
-n,attack = map(int,input().split()) # 방 개수, 용사 공격력
+n, attack = map(int,input().split())
+my_hp = 0
+max_hp = 0
 room = []
+
 for _ in range(n):
     t,a,h = map(int,input().split())
-    # t == 1 공격력 a, 생명력 h 인 몬스터가 존재
-    # t == 2 용사 공격력 a증가 생명력 h 만큼 회복
     room.append((t,a,h))
+
+monster = room[-1][2]
+
+for info in room :
+    if info[0] == 2 :
+        attack += info[1]
+        my_hp += info[2]
+    else :
+        monster_a = info[1]
+        monster_hp = info[2]
+        k = monster_hp%attack
+        if k != 0 :
+            damage = monster_a * (monster_hp//attack)
+        else:
+            damage = monster_a * (monster_hp//attack - 1)
+
+        my_hp -= damage
+    if my_hp > 0 :
+        my_hp = 0
+    max_hp = max(max_hp,abs(my_hp))
+
+print(max_hp+1)
+
