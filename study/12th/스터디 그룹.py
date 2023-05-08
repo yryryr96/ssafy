2975912e18f25d94ba1875af21be609c1a7d3e43
정영록 <s9285s@naver.com>
2023-05-08 21:28:39 +0900

    study

---
+++ study/12th/스터디 그룹.py
@@ -0,0 +1,37 @@
+import sys
+input = sys.stdin.readline
+
+N,K,D = map(int,input().split())
+students = []
+algorithm = [0]*(K+1)
+for _ in range(N):
+    M,d = map(int,input().split())
+    student_info = [d]
+    student_algo = list(map(int,input().split()))
+    student_info.append(student_algo)
+    students.append(student_info)
+students.sort()
+# print(students)
+start,end,E = 0,0,0
+while start < N :
+    # print(end,E)
+    while end < N and students[end][0] - students[start][0] <= D :
+        for i in students[end][1] :
+            algorithm[i] += 1
+        end += 1
+
+    total = every = 0
+    for i in range(1,K+1):
+        if algorithm[i] != 0 :
+            total += 1
+            if algorithm[i] == end - start :
+                every += 1
+    # print((total - every) * (end-start))
+    E = max(E,(total - every) * (end-start))
+    for i in students[start][1]:
+        algorithm[i] -= 1
+    start += 1
+
+print(E)
+
+
